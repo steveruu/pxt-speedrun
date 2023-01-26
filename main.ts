b81db47c049847c07cea08ab166df10659239ae4
steveruu <39307626+steveruu@users.noreply.github.com>
2023-01-26 10:20:17 +0100

    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-let actualCode = 12;
-let mySerial = Utility.encodeSerial()
-let actualGroup = 5
-let nextGroup = 0
-let nextCode = 0
-let blok1 = false
-let blok2 = false
+let codeValue = 12;
+let encodedSerial = Utility.encodeSerial();
+let actualGroup = 5;
+let nextGroup = 0;
+let nextCode = 0;
+
+let blok1 = false;
+let blok2 = false;
 
 radio.setTransmitPower(7);
 radio.setFrequencyBand(7);
@@ -12,33 +13,33 @@ radio.setTransmitSerialNumber(true);
 radio.setGroup(actualGroup);
 
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(actualCode);
+    radio.sendNumber(codeValue);
     basic.showString("A");
-    basic.clearScreen()
+    basic.clearScreen();
 })
 
 radio.onReceivedValue(function (key: string, value: number) {
-    if (mySerial === key) {
-        nextCode = value
-        blok1 = true
-        basic.showString("C")
-        basic.clearScreen()
+    if (encodedSerial === key) {
+        nextCode = value;
+        blok1 = true;
+        basic.showString("C");
+        basic.clearScreen();
 
     }
     if (key === "grp" || "grp:") {
-        nextGroup = value
-        blok2 = true
-        basic.showString("G")
-        basic.clearScreen()
+        nextGroup = value;
+        blok2 = true;
+        basic.showString("G");
+        basic.clearScreen();
 
     }
     if (blok1 && blok2) {
-        actualCode = nextCode
-        actualGroup = nextGroup
-        basic.showString("DONE")
-        basic.clearScreen()
-        blok1 = false
-        blok2 = false
+        codeValue = nextCode;
+        actualGroup = nextGroup;
+        basic.showString("D");
+        basic.clearScreen();
+        blok1 = false;
+        blok2 = false;
     }
 
 })
@@ -46,13 +47,13 @@ radio.onReceivedValue(function (key: string, value: number) {
 
 
 input.onButtonPressed(Button.AB, function () {
-    actualCode = 12;
-    mySerial = Utility.encodeSerial()
-    actualGroup = 5
-    nextGroup = 0
-    nextCode = 0
-    blok1 = false
-    blok2 = false
+    codeValue = 12;
+    encodedSerial = Utility.encodeSerial();
+    actualGroup = 5;
+    nextGroup = 0;
+    nextCode = 0;
+    blok1 = false;
+    blok2 = false;
     basic.showString("AB");
-    basic.clearScreen()
+    basic.clearScreen();
 })
